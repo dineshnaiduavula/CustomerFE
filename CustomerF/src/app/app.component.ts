7d8097a064bb3a67119fdf2b494e59cf99a4dc21
dinesh16vdoit <dinesh16.vdoit@gmail.com>
2024-08-11 12:06:36 +0530

    adding customer 1 grpahs

--- CustomerF/src/app/app.component.ts
+++ CustomerF/src/app/app.component.ts
@@ -1,13 +1,25 @@
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { NgxChartsModule } from '@swimlane/ngx-charts';
+import { async } from 'rxjs';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,HttpClientModule,NgxChartsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'CustomerF';
+  constructor(public a:HttpClient){}
+  data:any=[]
+  ngOnInit(){
+ this.a.get("http://localhost:1122/customer1").subscribe(async (datta:any)=>{
+     for(let i=0;i<datta.length;i++){
+     const r={name:datta[i].productname,value:datta[i].productValue}
+     this.data.push(r)
+    //  this.chartData = this.data.map((employee:any) => ({name: employee.productname,
+    //   value: employee.productValue }))
+    }})
+  }
 }
