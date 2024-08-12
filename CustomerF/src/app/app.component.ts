fa80a64fb8e84fcb299133b51ff56484e3b7bb7a
dinesh16vdoit <dinesh16.vdoit@gmail.com>
2024-08-12 12:32:38 +0530

    logic changes of this

--- CustomerF/src/app/app.component.ts
+++ CustomerF/src/app/app.component.ts
@@ -17,19 +17,53 @@ export class AppComponent {
   data3:any=[]
   ngOnInit(){
  this.a.get("http://localhost:1122/customer1").subscribe(async (datta:any)=>{
+
      for(let i=0;i<datta.length;i++){
+      let to=0
      const r={name:datta[i].productname,value:datta[i].productValue}
-     this.data1.push(r)
+     if(this.data1.length==0){ to=1
+      this.data1.push(r)}
+else{
+  for(let k=0;k<this.data1.length;k++){
+    if(this.data1[k].name==r.name){
+       to=1;
+       r.value=r.value+this.data1[k].value
+       this.data1[k]=r
+       break}}}
+     if(to==0){this.data1.push(r)}
+
     //  this.chartData = this.data.map((employee:any) => ({name: employee.productname,
     //   value: employee.productValue }))
     }})
     this.a.get("http://localhost:1122/customer2").subscribe(async (datta:any)=>{
       for(let i=0;i<datta.length;i++){
+        let to=0
       const r={name:datta[i].productname,value:datta[i].productValue}
-      this.data2.push(r)}})
+      if(this.data2.length==0){to=1
+        this.data2.push(r)}
+      else{
+        for(let k=0;k<this.data2.length;k++){
+          if(this.data2[k].name==r.name){
+             to=1;
+             r.value=r.value+this.data2[k].value
+             this.data2[k]=r
+             break}}}
+           if(to==0){this.data2.push(r)}
+    }})
       this.a.get("http://localhost:1122/customer3").subscribe(async (datta:any)=>{
         for(let i=0;i<datta.length;i++){
         const r={name:datta[i].productname,value:datta[i].productValue}
-        this.data3.push(r)}})
+        let to=0
+        if(this.data3.length==0){to=1
+          this.data3.push(r)}
+        else{
+          for(let k=0;k<this.data3.length;k++){
+            if(this.data3[k].name==r.name){
+               to=1;
+               r.value=r.value+this.data3[k].value
+               this.data3[k]=r
+               break}}}
+             if(to==0){this.data3.push(r)}
+      }})
   }
 }
