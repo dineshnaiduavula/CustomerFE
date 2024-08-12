eb2abe58e38da1e91120e4093779c08e1c2879d4
dinesh16vdoit <dinesh16.vdoit@gmail.com>
2024-08-12 10:21:23 +0530

    commit some changes

--- CustomerF/src/app/app.component.ts
+++ CustomerF/src/app/app.component.ts
@@ -12,14 +12,24 @@ import { async } from 'rxjs';
 })
 export class AppComponent {
   constructor(public a:HttpClient){}
-  data:any=[]
+  data1:any=[]
+  data2:any=[]
+  data3:any=[]
   ngOnInit(){
  this.a.get("http://localhost:1122/customer1").subscribe(async (datta:any)=>{
      for(let i=0;i<datta.length;i++){
      const r={name:datta[i].productname,value:datta[i].productValue}
-     this.data.push(r)
+     this.data1.push(r)
     //  this.chartData = this.data.map((employee:any) => ({name: employee.productname,
     //   value: employee.productValue }))
     }})
+    this.a.get("http://localhost:1122/customer2").subscribe(async (datta:any)=>{
+      for(let i=0;i<datta.length;i++){
+      const r={name:datta[i].productname,value:datta[i].productValue}
+      this.data2.push(r)}})
+      this.a.get("http://localhost:1122/customer3").subscribe(async (datta:any)=>{
+        for(let i=0;i<datta.length;i++){
+        const r={name:datta[i].productname,value:datta[i].productValue}
+        this.data3.push(r)}})
   }
 }
